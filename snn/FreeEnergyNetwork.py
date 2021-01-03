f723f39cf06fc789405f21b4f4a8a57ba9e6a8db
D Bersan <dbersann@gmail.com>
2021-01-03 17:33:24 +0100

    Add recording neurons

--- snn/FreeEnergyNetwork.py
+++ snn/FreeEnergyNetwork.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math 
+import matplotlib.pyplot as plt
 
 # activation: sigmoid
 def xF(x):
@@ -32,18 +33,29 @@ class FreeEnergyNetwork:
         self.T              = []
         self.t              = 0
         self.record         = False
-        self.record_neurons_list = []
+
+        self.unlock_timestamps = []
+        self.lock_timestamps   = []
+
+        # List of neurons to be recorded 
+        self.record_neurons_list_x = []
+        self.record_neurons_list_e = []
+
+        # Recorded data
+        self.recorded_data_x = {}
+        self.recorded_data_e = {}
+        self.recorded_error = []
 
         for l in range(self.layers):
             # X(layer, neuron) = self.X[layer, neuron-1]
-            self.X[l] = np.random.rand(neurons_per_layer[l])
+            self.X[l] = np.random.rand(self.neurons_per_layer[l])
 
         for l in range(self.layers-1):
             # E(layer, neuron) = self.E[layer, neuron-1]
-            self.E[l]       = np.random.rand(neurons_per_layer[l])
+            self.E[l]       = np.random.rand(self.neurons_per_layer[l])
 
             # Theta(layer, n1, n2) = self.Theta[layer-1, n1-1, n2-1]
-            self.Theta[l]   = np.random.rand(neurons_per_layer[l], neurons_per_layer[l+1])
+            self.Theta[l]   = np.random.rand(self.neurons_per_layer[l], self.neurons_per_layer[l+1])
 
         self.Sigma   = 1
         self.X_rate  = 0.3
@@ -115,7 +127,7 @@ class FreeEnergyNetwork:
                 for j in range(1, self.neurons_per_layer[l]+1):
                     deltaTheta = self.Theta_rate * self.dTheta(l, i, j)
                     self.incrementTheta(l, i ,j, deltaTheta)
-    
+
     def getX(self, layer, neuron):
         return self.X[layer][neuron-1]
 
@@ -152,12 +164,72 @@ class FreeEnergyNetwork:
     def dTheta(self, layer, i, j):
         return self.getE(layer-1, i) * F(self.getX(layer, j))
 
-    def record_neurons(self, neuron_list):
+    def record_neurons(self, neuron_list_x, neuron_list_e):
         if not self.record:
             self.record = True
-            for layer,index in neuron_list:
+            for layer,index in neuron_list_x:
                 assert layer>= 0 and layer < len(self.neurons_per_layer)
                 assert index>=1 and index-1 < self.neurons_per_layer[layer]
-                self.record_neurons_list.append((layer,index))
-    
-    
+                self.record_neurons_list_x.append((layer,index))
+
+            for layer,index in neuron_list_e:
+                assert layer>= 0 and layer < len(self.neurons_per_layer)-1
+                assert index>=1 and index-1 < self.neurons_per_layer[layer]
+                self.record_neurons_list_e.append((layer,index))
+
+            self.reset_record_neurons_lists()
+
+    def reset_record_neurons_lists(self):
+        self.T = []
+        self.t = 0
+        for neuron_index in self.record_neurons_list_x:
+            self.recorded_data_x[neuron_index] = []
+
+        for neuron_index in self.record_neurons_list_e:
+            self.recorded_data_e[neuron_index] = []
+
+        self.recorded_error = []
+        self.unlock_timestamps = []
+        self.lock_timestamps   = []
+
+    def network_snapshot(self):
+
+        # Record time
+        self.T.append(self.t)
+
+        # Record selected neurons
+        for layer, i in self.recorded_data_x:
+            self.recorded_data_x[(layer, i)].append(self.getX(layer,i))
+        
+        for layer, i in self.recorded_data_e:
+            self.recorded_data_e[(layer, i)].append(self.getE(layer,i))
+
+        # Record overall neuronal error
+        network_error = 0
+        for l in range(self.layers-1):
+            network_error += sum(self.E[l])
+
+        self.recorded_error.append(network_error)
+
+    def plot_recording(self):
+        # Plots recorded neurons and then resets them
+        plt.plot(self.T, self.recorded_error, color="red", label="error", linewidth=2.0)
+
+        for label in self.record_neurons_list_x:
+            plt.plot(self.T, self.recorded_data_x[label], '-', label=label, linewidth=3.0 )
+        
+        for label in self.record_neurons_list_e:
+            plt.plot(self.T, self.recorded_data_e[label], '--', label=label, linewidth=2.0 )
+
+        plt.legend(loc="upper left")
+        plt.ylabel('neuronal activity')
+        plt.xlabel('time')
+
+        for xc in self.unlock_timestamps: # black = unlock times
+            plt.axvline(x=xc, color='k', linestyle='--')
+
+        for xc in self.lock_timestamps: # blue = lock times
+            plt.axvline(x=xc, color='blue', linestyle='--')
+
+        plt.show()
+        self.reset_record_neurons_lists()
