b60ffe62906279663385984ba824387111f707c8
D Bersan <dhiegomaga@gmail.com>
2021-04-03 14:14:41 +0200

    Added imagenet pc train

---
+++ examples/features-pc-imagenet.py
@@ -0,0 +1,71 @@
+import tensorflow
+import numpy as np
+import pickle
+
+import sys
+sys.path.insert(0,'..')
+from snn.PcTorch import PcTorch
+from snn import util
+
+# Dataset Parameters
+NUM_CLASSES = 20
+FEATURES_PATH = '../feature-extractor/imagenet-features.p'
+
+# Train parameters
+NETWORK_ARCHITECTURE = [512,500,NUM_CLASSES]
+BATCH_SIZE = 16
+EPOCHS = 3
+DATA_PERC = 1.0
+INFERENCE_STEPS = 40
+OPTIMIZER = 'adam'  
+ACTIVATION='sigmoid'
+
+# Load features
+file = open(FEATURES_PATH,'rb')
+features = pickle.load(file)
+x_train = features['x_train_features']
+x_test = features['x_test_features']
+y_train = features['y_train']
+y_test = features['y_test']
+
+# One hot encode labels
+# (features already one hot encoded)
+
+# turn labels into list
+y_train_list = []
+y_test_list = []
+for i in range(len(y_train)):
+    y_train_list.append(y_train[i].flatten())
+
+for i in range(len(y_test)):
+    y_test_list.append(y_test[i].flatten())
+
+# turn features into list
+x_train_list = []
+x_test_list = []
+for i in range(len(x_train)):
+    x_train_list.append(x_train[i].flatten().astype(np.float))
+
+for i in range(len(x_test)):
+    x_test_list.append(x_test[i].flatten().astype(np.float))
+
+# normalize dataset
+x_train_list, mi, ma = util.normalize_dataset(x_train_list)
+x_test_list, mi, ma = util.normalize_dataset(x_test_list)
+
+# Initialize network and train
+model_torch = PcTorch(NETWORK_ARCHITECTURE)
+model_torch.train(
+    x_train_list, 
+    y_train_list, 
+    x_test_list, 
+    y_test_list, 
+    batch_size=BATCH_SIZE, 
+    epochs=EPOCHS, 
+    max_it=INFERENCE_STEPS,
+    optmizer=OPTIMIZER, 
+    activation=ACTIVATION,
+    dataset_perc = DATA_PERC
+)
+
+
---
+++ examples/pc-imagenet.py
@@ -0,0 +1,137 @@
+# Trains backprop on imagenet dataset 
+# Dataset link: http://www.image-net.org/download-images
+# Extracted .npz files should go inside dataset/ folder
+# RUN THIS FROM 'examples' folder
+
+import numpy as np
+from matplotlib import pyplot as plt
+from sklearn.utils import shuffle
+
+# tensorflow etc
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
+import sys
+sys.path.insert(0,'..')
+from snn.PcTorch import PcTorch
+from snn import util
+
+# Dataset Parameters
+CLASSES = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+NUM_CLASSES = len(CLASSES)
+DATASET_BATCH_COUNT = 6
+SUB_MEAN=False # sigmoid doesn't work if subtract mean
+IMAGE_SIZE = 32
+VALID_PERC = 0.2
+
+# Train parameters
+NETWORK_ARCHITECTURE = [3072, 500, 500, NUM_CLASSES]
+BATCH_SIZE = 16
+EPOCHS=1
+DATA_PERC = 0.2
+INFERENCE_STEPS = 40
+OPTIMIZER = 'adam'  
+ACTIVATION='sigmoid'
+
+def select_classes(x,y, CLASSES):
+    indices = []
+    for label in CLASSES:
+        rows = [i for i, x in enumerate(y) if x == label]
+        indices.extend(rows)
+
+    x = x[indices, :]
+    y = y[indices]
+
+    return x, y
+
+# Load data
+dataset = None
+multiple_files = True
+y =np.zeros((0,))
+x = np.zeros((0,IMAGE_SIZE*IMAGE_SIZE*3))
+
+if multiple_files:
+    count = DATASET_BATCH_COUNT # set to 10 when training on the full dataset
+    prefix = 'train_data_batch_'
+    for i in range(1, count+1):
+        name = prefix + str(i) + '.npz'
+        dataset = np.load('../datasets/'+name)
+        x_batch = dataset['data']
+        y_batch = dataset['labels']
+
+        # Select only certain classes
+        x_batch,y_batch = select_classes(x_batch,y_batch, CLASSES)
+
+        y = np.concatenate([y, y_batch], axis=0)
+        x = np.concatenate([x, x_batch], axis=0)
+
+else:
+    dataset = np.load('../datasets/val_data.npz')
+    x = dataset['data']
+    y = dataset['labels']
+
+    # Select only certain classes
+    x,y = select_classes(x,y, CLASSES)
+
+# Subtract 1 from labels 
+y = np.array([i-1 for i in y])
+
+# Shuffle
+x, y = shuffle(x, y)
+
+# Subtract mean, normalize...
+x = x/np.float32(255)
+if SUB_MEAN:
+    mean_image = np.mean(x, axis=0)
+    x -= mean_image
+
+# Separate train and validation data
+x_train = []
+y_train = []
+x_valid = []
+y_valid = []
+if VALID_PERC <= 1.0 and VALID_PERC>0:
+    train_index = int(x.shape[0]*(1-VALID_PERC))
+    x_train = x[:train_index]
+    y_train = y[:train_index]
+    x_valid = x[train_index:]
+    y_valid = y[train_index:]
+
+# One hot encode y
+num_classes = len(CLASSES)
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_valid = keras.utils.to_categorical(y_valid, num_classes)
+
+# turn labels into list
+y_train_list = []
+y_valid_list = []
+for i in range(len(y_train)):
+    y_train_list.append(y_train[i].flatten())
+
+for i in range(len(y_valid)):
+    y_valid_list.append(y_valid[i].flatten())
+
+# Flatten images
+x_train_list = []
+x_valid_list = []
+for i in range(len(x_train)):
+    x_train_list.append(x_train[i].flatten().astype(np.float))
+
+for i in range(len(x_valid)):
+    x_valid_list.append(x_valid[i].flatten().astype(np.float))
+
+# Initialize network and train
+model_torch = PcTorch(NETWORK_ARCHITECTURE)
+model_torch.train(
+    x_train_list, 
+    y_train_list, 
+    x_valid_list, 
+    y_valid_list, 
+    batch_size=BATCH_SIZE, 
+    epochs=EPOCHS, 
+    max_it=INFERENCE_STEPS,
+    optmizer=OPTIMIZER,
+    activation=ACTIVATION,
+    dataset_perc = DATA_PERC
+)
