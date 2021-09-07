5f9a9f4b558ebc68323cdc27a58ab628f3146bff
bersan <d>
2021-09-07 16:13:15 +0200

    Added linked list processing

--- datasets/imagenet-64x64-reduced/process-dataset-linkedlist.py
+++ datasets/imagenet-64x64-reduced/process-dataset-linkedlist.py
@@ -1,7 +1,9 @@
 # Reduces the Imagenet 64x64 to a limited number of classes
+# Using Linked List for performance
 
 import numpy as np
 import datetime
+from collections import deque
 
 # Dataset
 DATASET_TRAIN_BATCH_COUNT = 10
@@ -12,9 +14,8 @@ SUFFIX = '.npz'
 IMAGE_SIZE = 64
 CHANNELS = 3
 T = 100
-DESIRED_CLASSES=list(range(T,T+20))
+DESIRED_CLASSES=list(range(T,T+20)) # There are about 1280 images per class
 
-# Process Dataset
 
 # Dataset train files
 file_paths_train = []
@@ -23,10 +24,9 @@ for i in range(1, DATASET_TRAIN_BATCH_COUNT+1):
     file_paths_train.append(file)
 
 # Read train dataset
-labels = np.zeros((0,), dtype=np.int)
-data = np.zeros((0,IMAGE_SIZE*IMAGE_SIZE*CHANNELS), dtype=np.uint8) # using uint8 to fit dataset in memory
+llist_label_data = deque()
 
-t_start = datetime.datetime.now()
+t_start     = datetime.datetime.now()
 t_last_batch = datetime.datetime.now()
 
 for dataset_index,path in enumerate(file_paths_train):
@@ -36,14 +36,21 @@ for dataset_index,path in enumerate(file_paths_train):
 
     for i in range(x_batch.shape[0]):
         if y_batch[i] in DESIRED_CLASSES:
-            labels = np.append(labels, y_batch[i])
-            data = np.concatenate((data, x_batch[i:i+1]), axis=0)
+            llist_label_data.append((y_batch[i], x_batch[i]))
 
     t_now = datetime.datetime.now()
     batch_processing_time = (t_now - t_last_batch )
     print("Batch [%d], processing time: %.2f (seconds)" % (dataset_index, batch_processing_time.total_seconds()))
     t_last_batch = t_now
-    
+
+items = len(llist_label_data)
+labels = np.zeros((items,), dtype=np.int)
+data = np.zeros((items,IMAGE_SIZE*IMAGE_SIZE*CHANNELS), dtype=np.uint8) 
+
+for i, (l, d) in enumerate(llist_label_data):
+    labels[i] = l
+    data[i] = d
+
 # Show total processing time
 t_end = datetime.datetime.now()
 total_time = (t_end - t_start )
