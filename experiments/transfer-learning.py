69ce8a755975e3d4ee1086a687c9597cb92c5e10
bersan <d>
2021-09-08 18:39:31 +0200

    Added support for transformers

--- experiments/transfer-learning.py
+++ experiments/transfer-learning.py
@@ -7,6 +7,7 @@ from sklearn.utils import shuffle
 import torch
 import torch.nn as nn
 import torchvision
+import torchvision.transforms as transforms
 import torchvision.models as models
 from torchinfo import summary
 
@@ -22,43 +23,77 @@ IMAGE_SIZE = 64
 NUM_CLASSES = 1000
 TRAIN_BATCH_SIZE = 32
 EPOCHS = 6
-# VALID_PERC = 0.2 # Not used now
+USE_REDUCED_DATASET = True
+# VALID_PERC = 0.2 # Not used now, validation data is just one of the batches
 
-# Dataset
-DATASET_TRAIN_BATCH_COUNT = 6
-DATASET_VALID_BATCH_COUNT = 1
-FOLDER = 'datasets/imagenet-64x64/'
-PREFIX = 'train_data_batch_'
-SUFFIX = '.npz'
-
-# Variables to hold dataset data
-y = np.zeros((0,))
-x = np.zeros((0,3,IMAGE_SIZE,IMAGE_SIZE))
+# Network Parameters
+FC_NEURONS = 2048
+HIDDEN_LAYERS = 2
+PRINT_EVERY_N_BATCHES = 2000
 
 # Dataset files
-file_paths_train = []
-for i in range(1, DATASET_TRAIN_BATCH_COUNT+1):
-    file = FOLDER+PREFIX+str(i)+SUFFIX
-    file_paths_train.append(file)
+FOLDER = 'datasets/imagenet-64x64/'
+SUFFIX = '.npz'
+FILE_PATHS_TRAIN = [
+    'train_data_batch_1',
+    'train_data_batch_2',
+    'train_data_batch_3',
+    'train_data_batch_4',
+    'train_data_batch_5',
+    'train_data_batch_6',
+    'train_data_batch_7',
+    'train_data_batch_8',
+    'train_data_batch_9',
+]
 
-file_paths_valid = []
-for i in range(DATASET_TRAIN_BATCH_COUNT+1, 
-    DATASET_TRAIN_BATCH_COUNT+DATASET_VALID_BATCH_COUNT+1):
-    file = FOLDER+PREFIX+str(i)+SUFFIX
-    file_paths_valid.append(file)
+FILE_PATHS_VALID = [
+    'train_data_batch_10'
+]
+
+if USE_REDUCED_DATASET:     # Use reduced dataset?
+    FOLDER = 'datasets/imagenet-64x64-reduced/'
+    FILE_PATHS_TRAIN = ['dataset_training_reduced_1']
+    FILE_PATHS_VALID = ['dataset_valid_reduced']
+    NUM_CLASSES = 20
+    FC_NEURONS = 256
+    PRINT_EVERY_N_BATCHES = 200
+
+
+
+# Make full path
+FILE_PATHS_TRAIN = [FOLDER+file+SUFFIX for file in FILE_PATHS_TRAIN]
+FILE_PATHS_VALID = [FOLDER+file+SUFFIX for file in FILE_PATHS_VALID]
+
+# Variables to hold dataset
+# y = np.zeros((0,))
+# x = np.zeros((0,3,IMAGE_SIZE,IMAGE_SIZE))
 
 # Datasets
 params = {'batch_size': TRAIN_BATCH_SIZE,
           'shuffle': True,
           'num_workers': 0}
 
+# Data transformer
+# transform must contain transforms.ToTensor(), or be omitted 
+transform = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.RandomHorizontalFlip(),
+        transforms.ColorJitter(brightness=.2, hue=.2),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+
+# Simpler transform
+transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+
 # Training data
-train_dataset = Dataset(file_paths_train, IMAGE_SIZE)
+train_dataset = Dataset(FILE_PATHS_TRAIN, IMAGE_SIZE, transform=transform)
 train_generator = torch.utils.data.DataLoader(train_dataset, **params)
 
 # Show data example
 def imshow(img):
-    #img = img / 2 + 0.5     # unnormalize
+    img = img / 2 + 0.5     # unnormalize
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
@@ -70,12 +105,13 @@ imshow(torchvision.utils.make_grid(data))
 # Valid data
 valid_dataset=None
 valid_generator=None
-if len(file_paths_valid) > 0:
-    valid_dataset = Dataset(file_paths_valid, IMAGE_SIZE)
+if len(FILE_PATHS_VALID) > 0:
+    valid_dataset = Dataset(FILE_PATHS_VALID, IMAGE_SIZE)
     valid_generator = torch.utils.data.DataLoader(valid_dataset, **params)
 
 
 # Pre-trained model for Transfer Learning
+
 # vgg16 = models.vgg16()
 resnet = models.resnet152()
 num_ftrs = resnet.fc.in_features # Number of features before FC
@@ -85,7 +121,9 @@ for p in resnet.parameters():
     p.requires_grad = False
 summary(resnet, input_size=(TRAIN_BATCH_SIZE, 3, IMAGE_SIZE, IMAGE_SIZE))
 
+
 # Fully connected layer model
+
 import torch.nn.functional as F
 import torch.optim as optim
 
@@ -95,39 +133,64 @@ class FcModel(nn.Module):
         super(FcModel, self).__init__()
 
         # 1 Layer
-        #self.fc1 = nn.Linear(num_ftrs, NUM_CLASSES) 
+        if HIDDEN_LAYERS == 1:
+            self.dropout1 = nn.Dropout(0.25)
+            self.fc1 = nn.Linear(num_ftrs, NUM_CLASSES) 
 
         # 2 Layers
-        self.fc1 = nn.Linear(num_ftrs, 2048) 
-        self.fc2 = nn.Linear(2048, NUM_CLASSES)
+        if HIDDEN_LAYERS == 2:
+            self.fc1 = nn.Linear(num_ftrs, FC_NEURONS) 
+            self.fc2 = nn.Linear(FC_NEURONS, NUM_CLASSES)
+            self.dropout1 = nn.Dropout(0.25)
+            self.dropout2 = nn.Dropout(0.5)
 
         # Extra layers ... 
-        # self.fc2 = nn.Linear(..., ...)
-        # self.fc3 = nn.Linear(..., NUM_CLASSES)
+        if HIDDEN_LAYERS == 3:
+            self.fc1 = nn.Linear(num_ftrs, FC_NEURONS) 
+            self.fc2 = nn.Linear(FC_NEURONS, FC_NEURONS) 
+            self.fc3 = nn.Linear(FC_NEURONS, NUM_CLASSES)
+            self.dropout1 = nn.Dropout(0.25)
+            self.dropout2 = nn.Dropout(0.5)
+            self.dropout3 = nn.Dropout(0.5)
 
     def forward(self, x):
         x = torch.flatten(x, 1) # flatten all dimensions except the batch dimension
-
+        
         # 1 Layer
-        #x = self.fc1(x)
+        if HIDDEN_LAYERS == 1:
+            x = self.dropout1(x)
+            x = self.fc1(x)
 
         # 2 Layers
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        if HIDDEN_LAYERS == 2:
+            # x = self.dropout1(x)
+            x = self.fc1(x)
+            x = F.relu(x)
+            x = self.dropout2(x)
+            x = self.fc2(x)
 
-        #x = F.relu(self.fc1(x)) # Extra layers ...
-        #x = F.relu(self.fc2(x)) 
-        #x = self.fc3(x)
+        if HIDDEN_LAYERS == 3:
+            x = self.dropout1(x)
+            x = self.fc1(x)
+            x = F.relu(x)
+            x = self.dropout2(x)
+            x = self.fc2(x)
+            x = F.relu(x)
+            x = self.dropout3(x)
+            x = self.fc3(x)
+
         return x
 
 model = FcModel()
 model.to(device) # Move model to device
 summary(model,input_size=(TRAIN_BATCH_SIZE,num_ftrs))
 
+
 # Loss and optmizer
+
 criterion = nn.CrossEntropyLoss()
 # optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+optimizer = optim.Adam(model.parameters(), lr=0.01)
 
 # Training with mini batch size = 32, takes about 1 min every 64K samples (=2K mini batches)
 # With ~1.2M samples, 1 epoch takes ~20 min. 
@@ -136,12 +199,16 @@ for epoch in range(EPOCHS):
     prediction_list = []
     labels_list = []
     for i, (data, labels) in enumerate(train_generator):
+
+        # Activate dropouts
+        model.train()
         
         # Normalize
-        data = (data-125.0)/125.0
+        # data = data.float()
+        # data = (data-125.0)/125.0
 
         # Get samples
-        data = data.to(device).float()
+        data = data.to(device)
         labels = labels.to(device)
         labels_one_hot = F.one_hot(labels, num_classes=NUM_CLASSES)
 
@@ -169,11 +236,11 @@ for epoch in range(EPOCHS):
         prediction_list.extend(list(max_index.to('cpu').numpy()))
         labels_list.extend(labels.to('cpu').numpy())
 
-        # print statistics
+        # Calculate evaluation metrics
         running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
+        if i % PRINT_EVERY_N_BATCHES == PRINT_EVERY_N_BATCHES-1:    # print every N mini-batches
 
-            # Accuracy 
+            # Training metrics 
             acc_metric = np.equal(prediction_list, labels_list).sum()*1.0/len(prediction_list)
 
             # Print Loss and Accuracy
@@ -183,7 +250,16 @@ for epoch in range(EPOCHS):
             prediction_list = []
             labels_list = []
 
-    # Test accuracy...
+            # Validation metrics
+            running_loss_valid = 0.0
+            prediction_list_valid = []
+            labels_list_valid = []
+
+            # for i, (data, labels) in enumerate(valid_generator):
+            #   Disable dropouts: model.eval()
+            #   ...
+
+    # TODO Test accuracy
 
 
 
--- snn/Dataset.py
+++ snn/Dataset.py
@@ -1,15 +1,16 @@
 import numpy as np
 import torch
-import torch.nn.functional as F
+import torchvision.transforms as transforms
 
 class Dataset(torch.utils.data.Dataset):
 
-    def __init__(self, file_paths, image_size):
+    def __init__(self, file_paths, image_size, transform=transforms.ToTensor()):
 
         # Variables to hold dataset data
         self.y = np.zeros((0,), dtype=np.int)
-        self.x = np.zeros((0,3,image_size,image_size), dtype=np.uint8) # using uint8 to fit dataset in memory
+        self.x = np.zeros((0, image_size, image_size, 3), dtype=np.uint8) # using uint8 to fit dataset in memory
         self.image_size = image_size
+        self.transform = transform
 
         # Read dataset
         for path in file_paths:
@@ -29,27 +30,26 @@ class Dataset(torch.utils.data.Dataset):
 
     def __getitem__(self, index):
         'Generates one sample of data'
-        return self.x[index], self.y[index]
+
+        return self.transform(self.x[index]), self.y[index]
 
 
-    def get_images(self, data, img_size, subtract_mean=False):
+    def get_images(self, data, img_size):
         # Returns the dataset with image format, instead of flat array
         # Useful for convolutional networks
 
         # Normalize
         # data = data/np.float32(255)
-        
-        if subtract_mean:
-            mean_image = np.mean(data, axis=0)
-            data -= mean_image
 
         img_size2 = img_size * img_size
 
         data = np.dstack((data[:, :img_size2], data[:, img_size2:2*img_size2], data[:, 2*img_size2:]))
-        # Tesnsorflow image shape
-        #data = data.reshape((data.shape[0], img_size, img_size, 3)).transpose(0, 1, 2, 3)
 
-        # Pytorch shape
-        data = data.reshape((data.shape[0], img_size, img_size, 3)).transpose(0, 3, 1, 2).astype(np.uint8)
+        # Tesnsorflow shape [batch_size, rows, cols, channels]
+        data = data.reshape((data.shape[0], img_size, img_size, 3)).transpose(0, 1, 2, 3)
+
+        # Pytorch shape [batch_size, channels, rows, cols] 
+        # Not necessary, image will be reshaped by transforms.ToTensor()
+        # data = data.reshape((data.shape[0], img_size, img_size, 3)).transpose(0, 3, 1, 2).astype(np.uint8)
 
         return data
