f858b41ead30ee026be0b60bd0334a15760e42c9
bersan <d>
2021-09-30 18:58:30 +0200

    Add experiments

--- experiments/ImageNet_50_classes_224x224/run.bash
+++ experiments/ImageNet_50_classes_224x224/run.bash
@@ -1,19 +1,125 @@
 echo "----------------------------------------"
+echo "              VARY DEPTH                "
+echo "----------------------------------------"
 echo "Experiment 1"
 sleep 5
-python -W ignore exps/exp1.py
+python -W ignore experiments/ImageNet_50_classes_224x224/vary_depth/experiment_1.py
 echo "----------------------------------------"
 echo "Experiment 2"
 sleep 5
-python -W ignore exps/exp2.py
+python -W ignore experiments/ImageNet_50_classes_224x224/vary_depth/experiment_2.py
 echo "----------------------------------------"
 echo "Experiment 3"
 sleep 5
-python -W ignore exps/exp3.py
+python -W ignore experiments/ImageNet_50_classes_224x224/vary_depth/experiment_3.py
 echo "----------------------------------------"
 echo "Experiment 4"
 sleep 5
-python -W ignore exps/exp4.py
+python -W ignore experiments/ImageNet_50_classes_224x224/vary_depth/experiment_4.py
+echo "Experiment 5"
+sleep 5
+python -W ignore experiments/ImageNet_50_classes_224x224/vary_depth/experiment_5.py
+echo "Experiment 6"
+sleep 5
+python -W ignore experiments/ImageNet_50_classes_224x224/vary_depth/experiment_6.py
+echo "Experiment 7"
+sleep 5
+python -W ignore experiments/ImageNet_50_classes_224x224/vary_depth/experiment_7.py
+echo "Experiment 8"
+sleep 5
+python -W ignore experiments/ImageNet_50_classes_224x224/vary_depth/experiment_8.py
+echo "Experiment 9"
+sleep 5
+python -W ignore experiments/ImageNet_50_classes_224x224/vary_depth/experiment_9.py
+echo "Experiment 10"
+sleep 5
+python -W ignore experiments/ImageNet_50_classes_224x224/vary_depth/experiment_10.py
+
+echo "----------------------------------------"
+echo "              VARY FC NEURONS           "
+echo "----------------------------------------"
+echo "Experiment 1"
+sleep 5
+python -W ignore experiments/ImageNet_50_classes_224x224/vary_fc_neurons/experiment_1.py
+echo "----------------------------------------"
+echo "Experiment 2"
+sleep 5
+python -W ignore experiments/ImageNet_50_classes_224x224/vary_fc_neurons/experiment_2.py
+echo "----------------------------------------"
+echo "Experiment 3"
+sleep 5
+python -W ignore experiments/ImageNet_50_classes_224x224/vary_fc_neurons/experiment_3.py
+echo "----------------------------------------"
+echo "Experiment 4"
+sleep 5
+python -W ignore experiments/ImageNet_50_classes_224x224/vary_fc_neurons/experiment_4.py
+echo "Experiment 5"
+sleep 5
+python -W ignore experiments/ImageNet_50_classes_224x224/vary_fc_neurons/experiment_5.py
+echo "Experiment 6"
+sleep 5
+python -W ignore experiments/ImageNet_50_classes_224x224/vary_fc_neurons/experiment_6.py
+echo "Experiment 7"
+sleep 5
+python -W ignore experiments/ImageNet_50_classes_224x224/vary_fc_neurons/experiment_7.py
+echo "Experiment 8"
+sleep 5
+python -W ignore experiments/ImageNet_50_classes_224x224/vary_fc_neurons/experiment_8.py
+echo "Experiment 9"
+sleep 5
+python -W ignore experiments/ImageNet_50_classes_224x224/vary_fc_neurons/experiment_9.py
+echo "Experiment 10"
+sleep 5
+python -W ignore experiments/ImageNet_50_classes_224x224/vary_fc_neurons/experiment_10.py
+
+
+echo "----------------------------------------"
+echo "              VARY LR                   "
+echo "----------------------------------------"
+echo "Experiment 1"
+sleep 5
+python -W ignore experiments/ImageNet_50_classes_224x224/vary_lr/experiment_1.py
+echo "----------------------------------------"
+echo "Experiment 2"
+sleep 5
+python -W ignore experiments/ImageNet_50_classes_224x224/vary_lr/experiment_2.py
+echo "----------------------------------------"
+echo "Experiment 3"
+sleep 5
+python -W ignore experiments/ImageNet_50_classes_224x224/vary_lr/experiment_3.py
+echo "----------------------------------------"
+echo "Experiment 4"
+sleep 5
+python -W ignore experiments/ImageNet_50_classes_224x224/vary_lr/experiment_4.py
+echo "Experiment 5"
+sleep 5
+python -W ignore experiments/ImageNet_50_classes_224x224/vary_lr/experiment_5.py
+echo "Experiment 6"
+sleep 5
+python -W ignore experiments/ImageNet_50_classes_224x224/vary_lr/experiment_6.py
+echo "Experiment 7"
+sleep 5
+python -W ignore experiments/ImageNet_50_classes_224x224/vary_lr/experiment_7.py
+
+echo "----------------------------------------"
+echo "      VARY OPTIMIZER/ACTIVATION         "
+echo "----------------------------------------"
+echo "Experiment 1"
+sleep 5
+python -W ignore experiments/ImageNet_50_classes_224x224/vary_optimizer_activation/experiment_1.py
+echo "----------------------------------------"
+echo "Experiment 2"
+sleep 5
+python -W ignore experiments/ImageNet_50_classes_224x224/vary_optimizer_activation/experiment_2.py
+echo "----------------------------------------"
+echo "Experiment 3"
+sleep 5
+python -W ignore experiments/ImageNet_50_classes_224x224/vary_optimizer_activation/experiment_3.py
+echo "----------------------------------------"
+echo "Experiment 4"
+sleep 5
+python -W ignore experiments/ImageNet_50_classes_224x224/vary_optimizer_activation/experiment_4.py
+
 
 
 
---
+++ experiments/ImageNet_50_classes_224x224/vary_lr/experiment_5.py
@@ -0,0 +1,231 @@
+# Transfer Learning on imagenet (full image sizes) dataset 
+# Set flag FOLDER to where the dataset is. It should contain a `train` and `val` folders inside
+
+import numpy as np
+from matplotlib import pyplot as plt
+from sklearn.utils import shuffle
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+import torchvision.models as models
+from torchinfo import summary
+
+import sys
+import os
+sys.path.append('.')
+from snn.PcTorch import PcTorch
+
+# Import util functions 
+import examples.ModelUtils as ModelUtils
+
+# Set PyTorch device
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Dataset Parameters
+IMAGE_SIZE = 224
+TRAIN_BATCH_SIZE = 32
+EPOCHS = 20
+VALID_PERC = 0.2 
+USE_REDUCED_DATASET = True
+
+# Network Architecture
+FC_NEURONS = 2048 # deprecated, set at the `parameters` dictionary
+PRINT_EVERY_N_BATCHES = 2000
+
+# Predictive Coding parameters
+INFERENCE_STEPS = 40
+
+# Dataset files
+FOLDER = '/data/datasets/imagenet/ILSVRC2012/train'
+SUFFIX = '.JPEG'
+
+if USE_REDUCED_DATASET:     # Use reduced dataset?
+    FOLDER = 'datasets/imagenet-reduced/train/'
+    FC_NEURONS = 256
+    PRINT_EVERY_N_BATCHES = 100
+
+# Tunnable hyper-parameters
+parameters = {
+        
+        # Common parameters
+        'optimizer': 'adam',
+        'activation': 'sigmoid', 
+        'hidden_layers': 2, 
+        'fc_neurons': 256,
+
+        # Backprop
+        'dropout_bp': False, 
+        'lr_bp': 0.004, 
+        'momentum_bp': 0.9, 
+
+        # PC
+        'lr_pc': 0.004, 
+        'momentum_pc': 0.9 
+    } 
+
+# Count number of classes
+subfolders = [ f.path for f in os.scandir(FOLDER) if f.is_dir() ]
+NUM_CLASSES = len(subfolders)
+
+def load_split_train_test(datadir, valid_size = .2):
+    # Data transformer
+    # transform must contain transforms.ToTensor(), or be omitted 
+    mean = 0.5
+    std = 0.5
+
+    # TODO resize image to common NN input size (random crop, etc)
+    train_transform = transforms.Compose([
+            transforms.RandomResizedCrop(IMAGE_SIZE, scale=(0.65, 1.0), ratio=(0.9, 1.1)),
+            transforms.RandomHorizontalFlip(),
+            transforms.ColorJitter(brightness=.3, hue=.05, contrast=0.2),
+            transforms.ToTensor(),
+            transforms.Normalize([mean, mean, mean], [std, std, std])])
+
+    # Simpler transform
+    valid_transform = transforms.Compose([
+            transforms.CenterCrop(IMAGE_SIZE),
+            transforms.ToTensor(),
+            transforms.Normalize([mean, mean, mean], [std, std, std])])
+                                      
+    train_data = torchvision.datasets.ImageFolder(datadir,       
+                    transform=train_transform)
+    valid_data = torchvision.datasets.ImageFolder(datadir,
+                    transform=valid_transform)
+
+    num_train = len(train_data)
+    indices = list(range(num_train))
+    split = int(np.floor(valid_size * num_train))
+    np.random.shuffle(indices)
+    from torch.utils.data.sampler import SubsetRandomSampler
+    train_idx, valid_idx = indices[split:], indices[:split]
+
+    train_sampler = SubsetRandomSampler(train_idx)
+    valid_sampler = SubsetRandomSampler(valid_idx)
+    train_generator = torch.utils.data.DataLoader(train_data,
+                   batch_size=TRAIN_BATCH_SIZE, sampler=train_sampler, drop_last=True)
+    valid_generator = torch.utils.data.DataLoader(valid_data,
+                   batch_size=TRAIN_BATCH_SIZE, sampler=valid_sampler, drop_last=True)
+    return train_generator, valid_generator
+
+# Data generators
+train_generator, valid_generator = load_split_train_test(FOLDER, VALID_PERC)
+
+# Show data example
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+train_it = train_generator.__iter__()
+data,labels = next(train_it)
+#imshow(torchvision.utils.make_grid(data))
+
+
+# Resnet
+# resnet = models.resnet152(pretrained=True)
+# num_ftrs_resnet = resnet.fc.in_features # Number of features before FC
+# modules = list(resnet.children())[:-1]
+# resnet = nn.Sequential(*modules)
+# for p in resnet.parameters():
+#     p.requires_grad = False
+
+resnet = models.resnet152(pretrained=True)
+num_ftrs_resnet = resnet.fc.in_features
+for param in resnet.parameters():
+    param.requires_grad = False
+resnet.fc = nn.Flatten()
+
+# Vgg16
+vgg16 = models.vgg16(pretrained=True)
+vgg16 = vgg16.features
+for p in vgg16.parameters():
+    p.requires_grad = False
+num_ftrs_vgg16 = 512*7*7
+
+# Choose extractor
+feature_extractor = resnet
+num_ftrs = num_ftrs_resnet
+
+feature_extractor = feature_extractor.to(device)
+# summary(feature_extractor, input_size=(TRAIN_BATCH_SIZE, 3, IMAGE_SIZE, IMAGE_SIZE))
+
+
+# Fully connected layer model
+model = ModelUtils.getFcModel(  num_ftrs, 
+                                NUM_CLASSES, 
+                                parameters['hidden_layers'], 
+                                parameters['fc_neurons'], 
+                                parameters['activation'],
+                                parameters['dropout_bp'] )
+
+model.to(device) # Move model to device
+# summary(model,input_size=(TRAIN_BATCH_SIZE,num_ftrs))
+
+# Predictive Coding model
+pc_model_architecture = ModelUtils.getPcModelArchitecture(
+    num_ftrs,
+    NUM_CLASSES,
+    parameters['hidden_layers'],
+    parameters['fc_neurons']
+)
+
+pc_model = PcTorch(pc_model_architecture)
+pc_model.set_training_parameters(
+    TRAIN_BATCH_SIZE,
+    INFERENCE_STEPS, 
+    parameters['activation'], 
+    parameters['optimizer'], 
+    parameters['lr_pc'],
+    parameters['momentum_pc'],
+    normalize_input=True)
+
+
+# Loss and optmizer
+criterion = nn.CrossEntropyLoss()
+optimizer = 'None'
+
+if parameters['optimizer'] == 'sgd':
+    optimizer = optim.SGD(model.parameters(), 
+        lr=parameters['lr_bp'], 
+        momentum=parameters['momentum_bp'])
+
+elif parameters['optimizer'] == 'adam':
+    optimizer = optim.Adam(model.parameters(), 
+        lr=parameters['lr_bp'], 
+        betas=(parameters['momentum_bp'], 0.999))
+
+# Train models
+metrics = ModelUtils.train_TransferLearning_Simultaneous_Backprop_PC(
+            EPOCHS,
+            NUM_CLASSES,
+            train_generator,
+            valid_generator,
+            model,
+            feature_extractor,
+            criterion,
+            optimizer,
+            device,
+            PRINT_EVERY_N_BATCHES,
+            pc_model=pc_model)
+
+# Print Metrics
+ModelUtils.printMetrics(metrics)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
