3ed0d74b371d8a01c7e289ec95173b19712c6212
bersan <d>
2021-09-30 18:33:09 +0200

    Add experiments

---
+++ exps/ImageNet_50_classes_224x224/run.bash
@@ -0,0 +1,22 @@
+echo "----------------------------------------"
+echo "Experiment 1"
+sleep 5
+python -W ignore exps/exp1.py
+echo "----------------------------------------"
+echo "Experiment 2"
+sleep 5
+python -W ignore exps/exp2.py
+echo "----------------------------------------"
+echo "Experiment 3"
+sleep 5
+python -W ignore exps/exp3.py
+echo "----------------------------------------"
+echo "Experiment 4"
+sleep 5
+python -W ignore exps/exp4.py
+
+
+
+
+
+
