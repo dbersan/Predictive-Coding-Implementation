e18530cdee6e2cfae85e096d46f6ef8b206d25f2
D Bersan <dhiegomaga@gmail.com>
2021-10-07 20:02:28 +0200

    Create experiment 1000 classes

---
+++ experiments/ImageNet_1000_classes_224x224/run.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+echo "----------------------------------------"
+echo "        1000 classes experiment         "
+echo "----------------------------------------"
+echo "Experiment 1"
+sleep 5
+python experiments/ImageNet_1000_classes_224x224/experiment_1.py
+echo "----------------------------------------"
