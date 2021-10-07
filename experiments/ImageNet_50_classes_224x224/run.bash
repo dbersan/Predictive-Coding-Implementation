c71c5a53fa4e43ac325adae949b662b4be5873a0
bersan <d>
2021-10-07 18:07:53 +0200

    Update experiment file

--- experiments/ImageNet_50_classes_224x224/run.bash
+++ experiments/ImageNet_50_classes_224x224/run.bash
@@ -1,82 +1,83 @@
+#!/usr/bin/env bash
 # echo "----------------------------------------"
 # echo "              VARY DEPTH                "
 # echo "----------------------------------------"
 # echo "Experiment 1"
 # sleep 5
-# # python -W ignore experiments/ImageNet_50_classes_224x224/vary_depth/experiment_1.py <- too little layers, maybe a problem
+# # python experiments/ImageNet_50_classes_224x224/vary_depth/experiment_1.py <- too little layers, maybe a problem
 # echo "----------------------------------------"
 # echo "Experiment 2"
 # sleep 5
-# python -W ignore experiments/ImageNet_50_classes_224x224/vary_depth/experiment_2.py
+# python experiments/ImageNet_50_classes_224x224/vary_depth/experiment_2.py
 # echo "----------------------------------------"
 # echo "Experiment 3"
 # sleep 5
-# python -W ignore experiments/ImageNet_50_classes_224x224/vary_depth/experiment_3.py
+# python experiments/ImageNet_50_classes_224x224/vary_depth/experiment_3.py
 # echo "----------------------------------------"
 # echo "Experiment 4"
 # sleep 5
-# python -W ignore experiments/ImageNet_50_classes_224x224/vary_depth/experiment_4.py
+# python experiments/ImageNet_50_classes_224x224/vary_depth/experiment_4.py
 # echo "Experiment 5"
 # sleep 5
-# python -W ignore experiments/ImageNet_50_classes_224x224/vary_depth/experiment_5.py
+# python experiments/ImageNet_50_classes_224x224/vary_depth/experiment_5.py
 # echo "Experiment 6"
 # sleep 5
-# python -W ignore experiments/ImageNet_50_classes_224x224/vary_depth/experiment_6.py 
+# python experiments/ImageNet_50_classes_224x224/vary_depth/experiment_6.py 
 # echo "Experiment 7"
 # sleep 5
-# # python -W ignore experiments/ImageNet_50_classes_224x224/vary_depth/experiment_7.py <- too little layers, maybe a problem
+# # python experiments/ImageNet_50_classes_224x224/vary_depth/experiment_7.py <- too little layers, maybe a problem
 # echo "Experiment 8"
 # sleep 5
-# python -W ignore experiments/ImageNet_50_classes_224x224/vary_depth/experiment_8.py
+# python experiments/ImageNet_50_classes_224x224/vary_depth/experiment_8.py
 # echo "Experiment 9"
 # sleep 5
-# python -W ignore experiments/ImageNet_50_classes_224x224/vary_depth/experiment_9.py
-echo "Experiment 10"
-sleep 5
-python -W ignore experiments/ImageNet_50_classes_224x224/vary_depth/experiment_10.py
-echo "Experiment 11"
-sleep 5
-python -W ignore experiments/ImageNet_50_classes_224x224/vary_depth/experiment_11.py
-echo "Experiment 12"
-sleep 5
-python -W ignore experiments/ImageNet_50_classes_224x224/vary_depth/experiment_12.py
+# python experiments/ImageNet_50_classes_224x224/vary_depth/experiment_9.py
+# echo "Experiment 10"
+# sleep 5
+# python experiments/ImageNet_50_classes_224x224/vary_depth/experiment_10.py
+# echo "Experiment 11"
+# sleep 5
+# python experiments/ImageNet_50_classes_224x224/vary_depth/experiment_11.py
+# echo "Experiment 12"
+# sleep 5
+# python experiments/ImageNet_50_classes_224x224/vary_depth/experiment_12.py
 
-echo "----------------------------------------"
-echo "              VARY FC NEURONS           "
-echo "----------------------------------------"
-echo "Experiment 1"
-sleep 5
-python -W ignore experiments/ImageNet_50_classes_224x224/vary_fc_neurons/experiment_1.py
-echo "----------------------------------------"
-echo "Experiment 2"
-sleep 5
-python -W ignore experiments/ImageNet_50_classes_224x224/vary_fc_neurons/experiment_2.py
-echo "----------------------------------------"
-echo "Experiment 3"
-sleep 5
-python -W ignore experiments/ImageNet_50_classes_224x224/vary_fc_neurons/experiment_3.py
-echo "----------------------------------------"
-echo "Experiment 4"
-sleep 5
-python -W ignore experiments/ImageNet_50_classes_224x224/vary_fc_neurons/experiment_4.py
-echo "Experiment 5"
-sleep 5
-python -W ignore experiments/ImageNet_50_classes_224x224/vary_fc_neurons/experiment_5.py
-echo "Experiment 6"
-sleep 5
-python -W ignore experiments/ImageNet_50_classes_224x224/vary_fc_neurons/experiment_6.py
+# echo "----------------------------------------"
+# echo "              VARY FC NEURONS           "
+# echo "----------------------------------------"
+# echo "Experiment 1"
+# sleep 5
+# python experiments/ImageNet_50_classes_224x224/vary_fc_neurons/experiment_1.py
+# echo "----------------------------------------"
+# echo "Experiment 2"
+# sleep 5
+# python experiments/ImageNet_50_classes_224x224/vary_fc_neurons/experiment_2.py
+# echo "----------------------------------------"
+# echo "Experiment 3"
+# sleep 5
+# python experiments/ImageNet_50_classes_224x224/vary_fc_neurons/experiment_3.py
+# echo "----------------------------------------"
+# echo "Experiment 4"
+# sleep 5
+# python experiments/ImageNet_50_classes_224x224/vary_fc_neurons/experiment_4.py
+# echo "Experiment 5"
+# sleep 5
+# python experiments/ImageNet_50_classes_224x224/vary_fc_neurons/experiment_5.py
+# echo "Experiment 6"
+# sleep 5
+# python experiments/ImageNet_50_classes_224x224/vary_fc_neurons/experiment_6.py
 echo "Experiment 7"
 sleep 5
-python -W ignore experiments/ImageNet_50_classes_224x224/vary_fc_neurons/experiment_7.py
+python experiments/ImageNet_50_classes_224x224/vary_fc_neurons/experiment_7.py
 echo "Experiment 8"
 sleep 5
-python -W ignore experiments/ImageNet_50_classes_224x224/vary_fc_neurons/experiment_8.py
+python experiments/ImageNet_50_classes_224x224/vary_fc_neurons/experiment_8.py
 echo "Experiment 9"
 sleep 5
-python -W ignore experiments/ImageNet_50_classes_224x224/vary_fc_neurons/experiment_9.py
+python experiments/ImageNet_50_classes_224x224/vary_fc_neurons/experiment_9.py
 echo "Experiment 10"
 sleep 5
-python -W ignore experiments/ImageNet_50_classes_224x224/vary_fc_neurons/experiment_10.py
+python experiments/ImageNet_50_classes_224x224/vary_fc_neurons/experiment_10.py
 
 
 echo "----------------------------------------"
@@ -84,47 +85,47 @@ echo "              VARY LR                   "
 echo "----------------------------------------"
 echo "Experiment 1"
 sleep 5
-python -W ignore experiments/ImageNet_50_classes_224x224/vary_lr/experiment_1.py
+python experiments/ImageNet_50_classes_224x224/vary_lr/experiment_1.py
 echo "----------------------------------------"
 echo "Experiment 2"
 sleep 5
-python -W ignore experiments/ImageNet_50_classes_224x224/vary_lr/experiment_2.py
+python experiments/ImageNet_50_classes_224x224/vary_lr/experiment_2.py
 echo "----------------------------------------"
 echo "Experiment 3"
 sleep 5
-python -W ignore experiments/ImageNet_50_classes_224x224/vary_lr/experiment_3.py
+python experiments/ImageNet_50_classes_224x224/vary_lr/experiment_3.py
 echo "----------------------------------------"
 echo "Experiment 4"
 sleep 5
-python -W ignore experiments/ImageNet_50_classes_224x224/vary_lr/experiment_4.py
+python experiments/ImageNet_50_classes_224x224/vary_lr/experiment_4.py
 echo "Experiment 5"
 sleep 5
-python -W ignore experiments/ImageNet_50_classes_224x224/vary_lr/experiment_5.py
+python experiments/ImageNet_50_classes_224x224/vary_lr/experiment_5.py
 echo "Experiment 6"
 sleep 5
-python -W ignore experiments/ImageNet_50_classes_224x224/vary_lr/experiment_6.py
+python experiments/ImageNet_50_classes_224x224/vary_lr/experiment_6.py
 echo "Experiment 7"
 sleep 5
-python -W ignore experiments/ImageNet_50_classes_224x224/vary_lr/experiment_7.py
+python experiments/ImageNet_50_classes_224x224/vary_lr/experiment_7.py
 
 echo "----------------------------------------"
 echo "      VARY OPTIMIZER/ACTIVATION         "
 echo "----------------------------------------"
 echo "Experiment 1"
 sleep 5
-python -W ignore experiments/ImageNet_50_classes_224x224/vary_optimizer_activation/experiment_1.py
+python experiments/ImageNet_50_classes_224x224/vary_optimizer_activation/experiment_1.py
 echo "----------------------------------------"
 echo "Experiment 2"
 sleep 5
-python -W ignore experiments/ImageNet_50_classes_224x224/vary_optimizer_activation/experiment_2.py
+python experiments/ImageNet_50_classes_224x224/vary_optimizer_activation/experiment_2.py
 echo "----------------------------------------"
 echo "Experiment 3"
 sleep 5
-python -W ignore experiments/ImageNet_50_classes_224x224/vary_optimizer_activation/experiment_3.py
+python experiments/ImageNet_50_classes_224x224/vary_optimizer_activation/experiment_3.py
 echo "----------------------------------------"
 echo "Experiment 4"
 sleep 5
-python -W ignore experiments/ImageNet_50_classes_224x224/vary_optimizer_activation/experiment_4.py
+python experiments/ImageNet_50_classes_224x224/vary_optimizer_activation/experiment_4.py
 
 
 
